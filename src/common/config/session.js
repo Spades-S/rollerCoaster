3a86624f422496eea2d6e0f9843282504a810cb9
Spades-S <allengefeng@163.com>
2017-06-13 09:06:42 +0800

    framework of rollercoaster

---
+++ src/common/config/session.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * session configs
+ */
+export default {
+  name: 'thinkjs',
+  type: 'file',
+  secret: 'W4S6R31C',
+  timeout: 24 * 3600,
+  cookie: { // cookie options
+    length: 32,
+    httponly: true
+  },
+  adapter: {
+    file: {
+      path: think.RUNTIME_PATH + '/session',
+    }
+  }
+};
